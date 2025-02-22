debb0b569859911d7094c68a1a04d19c0e85f2b0
Suman <sumankamilya27@gmail.com>
2025-02-22 19:24:28 +0530

    Updated config file to import env properly

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import API_BASE_URL from "../../src/config";
+import API_BASE_URL from "../conf/config";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
